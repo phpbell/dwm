ea939bbf69923202f495453a43c4c42e50f3f4b8
PHPBell <79753816+phpbell@users.noreply.github.com>
2021-03-21 09:00:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# dwm 6.2
+## Instalar
+    git clone https://github.com/phpbell/dwm.git && cd dwm && sh make
+
+## Configurações
+    nano config.h
+
+## .profile
+
+```bash
+# resolve o bug de apps java
+export _JAVA_AWT_WM_NONREPARENTING=1 
+
+# bateria, memória, calendário e relógio
+while true; do
+    bat="$(acpi -b)"
+    mem="$(free -h --si | awk '(NR==2){ print $3 }')"
+    status="$(echo "$bat, Mem: $mem ~" && date +"%A, %d.%b.%Y %H:%M")"
+    xsetroot -name " $(echo $status | xargs) "
+    sleep 1s    # atualiza a cada 1 segundo
+done &
+
+# bateria, memória, calendário e relógio (versão PHP)
+while true; do
+    bat="$(acpi -b)"
+    mem="$(free -h --si | awk '(NR==2){ print $3 }')"
+    data="$(php /opt/home/www/html/data.php)"
+    status="$(echo "$bat, Mem: $mem ~ $data")"
+    xsetroot -name " $(echo $status | xargs) "
+    sleep 1s    # atualiza a cada 1 segundo
+done &
+
+# alarme
+setsid alarm-clock-applet &
+
+# wallpaper
+setsid nitrogen --restore &
---
+++ data.php
@@ -0,0 +1,76 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+$time=time();
+//https://www.php.net/manual/en/datetime.format.php
+$dia_por_extenso=date('N',$time);
+switch($dia_por_extenso){
+    case 1:
+    $dia_por_extenso='Segunda-feira';
+    break;
+    case 2:
+    $dia_por_extenso='Terça-feira';
+    break;
+    case 3:
+    $dia_por_extenso='Quarta-feira';
+    break;
+    case 4:
+    $dia_por_extenso='Quinta-feira';
+    break;
+    case 5:
+    $dia_por_extenso='Sexta-feira';
+    break;
+    case 6:
+    $dia_por_extenso='Sábado';
+    break;
+    case 7:
+    $dia_por_extenso='Domingo';
+    break;
+}
+$dia_por_extenso_em_ingles=date('l',$time);
+$dia=date('d',$time);
+$mes=date('n',$time);
+switch($mes){
+    case 1:
+    $mes='Jan';
+    break;
+    case 2:
+    $mes='Fev';
+    break;
+    case 3:
+    $mes='Mar';
+    break;
+    case 4:
+    $mes='Abr';
+    break;
+    case 5:
+    $mes='Mai';
+    break;
+    case 6:
+    $mes='Jun';
+    break;
+    case 7:
+    $mes='Jul';
+    break;
+    case 8:
+    $mes='Ago';
+    break;
+    case 9:
+    $mes='Set';
+    break;
+    case 10:
+    $mes='Out';
+    break;
+    case 11:
+    $mes='Nov';
+    break;
+    case 12:
+    $mes='Dez';
+    break;
+}
+$ano=date('Y',$time);
+$hora=date('H',$time);
+$minuto=date('i',$time);
+$str=$dia_por_extenso_em_ingles.'/'.$dia_por_extenso.', ';
+$str.=$dia.'.'.$mes.'.'.$ano.' ';
+$str.=$hora.':'.$minuto;
+print escapeshellcmd($str);
